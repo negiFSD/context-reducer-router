cc5cc5e4c18a48ecbb2c2dfed6582d148dbbb65e
negiFSD <sunilnegi217@rediffmail.com>
2022-06-17 17:33:15 +0530

    initial commit

---
+++ src/Component/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+            <h5>Company Name</h5>
+            <input type={'search'}></input>
+            <span> cart</span>
+    </div>
+  )
+}
+
+export default Header
